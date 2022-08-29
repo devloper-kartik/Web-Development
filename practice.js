911a8b81f9e1fb4c28f5f0bff63dfc581c2f4ed4
devloper-kartik <112373403+devloper-kartik@users.noreply.github.com>
2022-08-29 14:18:05 +0530

    Add files via upload

--- practice.js
+++ practice.js
@@ -1,13 +1,26 @@
-var list = document.getElementsByClassName("list-group-item");
+// var list = document.getElementsByClassName("list-group-item");
 
-var li = document.getElementsByTagName("li");
+// var li = document.getElementsByTagName("li");
 
-list[2].style.backgroundColor = "green";
+// list[2].style.backgroundColor = "green";
 
-for(var i=0;i<list.length;i++){
-    list[i].style.fontWeight = "bold";
-}
+// for(var i=0;i<list.length;i++){
+//     list[i].style.fontWeight = "bold";
+// }
 
-for(var i=0;i<li.length;i++){
-    li[i].style.backgroundColor = "grey";
+// for(var i=0;i<li.length;i++){
+//     li[i].style.backgroundColor = "grey";
+// }
+
+var list = document.querySelectorAll("li");
+// list[1].style.backgroundColor = "green";
+
+list[2].style.display = 'none'; 
+
+list[1].style.color = "blue";
+
+var oddList = document.querySelectorAll("li:nth-child(odd)");
+
+for(var i=0;i<oddList.length;i++){
+    oddList[i].style.backgroundColor = "green";
 }
