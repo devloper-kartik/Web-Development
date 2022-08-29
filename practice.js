44fb38fc1fda3ef70c4c9fe614673ff6d84cd296
devloper-kartik <112373403+devloper-kartik@users.noreply.github.com>
2022-08-29 13:40:32 +0530

    Add files via upload

--- practice.js
+++ practice.js
@@ -1,8 +1,13 @@
 var list = document.getElementsByClassName("list-group-item");
-var btn = document.getElementsByClassName("btn btn-dark");
-console.log(btn);
+
+var li = document.getElementsByTagName("li");
+
 list[2].style.backgroundColor = "green";
 
 for(var i=0;i<list.length;i++){
     list[i].style.fontWeight = "bold";
+}
+
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor = "grey";
 }
