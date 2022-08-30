a29e33a2abb5a03800bf2a995351a09d590796e2
devloper-kartik <112373403+devloper-kartik@users.noreply.github.com>
2022-08-30 13:26:26 +0530

    Search in discription

--- practice.js
+++ practice.js
@@ -1,18 +1,23 @@
 var form = document.getElementById("addForm");
 var itemList = document.getElementById("items");
+var filter  = document.getElementById("filter");
 
 form.addEventListener("submit",addfun);
 itemList.addEventListener("click",removeItem);
+filter.addEventListener("keyup",search);
 
 function addfun(e){
     e.preventDefault();
 
     var newItem =  document.getElementById('item').value;
+    var dis  = document.getElementById("disc").value;
     var li = document.createElement("li");
+    var next = document.createElement("br");
 
     li.className = "list-group-item";
     li.appendChild(document.createTextNode(newItem));
-
+    li.appendChild(next);
+    li.appendChild(document.createTextNode(dis));
     var delbtn = document.createElement("button");
     var editbtn = document.createElement("button");
 
@@ -20,13 +25,14 @@ function addfun(e){
     delbtn.appendChild(document.createTextNode("X"));
 
     editbtn.appendChild(document.createTextNode("Edit"));
-    editbtn.className = "btn btn-dark";
-    delbtn.nextElementSibling=editbtn;
+    editbtn.className = "btn btn-danger btn-sm float-right delete";
+    
 
     li.appendChild(delbtn);
-    li.appendChild(editbtn);
 
     itemList.appendChild(li);
+
+    li.insertBefore(editbtn,delbtn);
 }
 
 function removeItem(e){
@@ -36,4 +42,20 @@ function removeItem(e){
             itemList.removeChild(parent);
         }
     }
+}
+
+function search(e){
+    var text = e.target.value.toLowerCase();
+    var list = itemList.getElementsByTagName("li");
+
+    Array.from(list).forEach((item)=>{
+        // var name = item.firstChild.textContent;
+        var dis = item.childNodes;
+        console.log(dis);
+        if(dis[0].textContent.toLowerCase().indexOf(text) != -1 || dis[2].textContent.toLowerCase().indexOf(text) != -1 ){
+            item.style.display = 'block';
+        }else{
+            item.style.display = "none";
+        }
+    });
 }
