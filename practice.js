02b7425816c20262d310eaf2264198f77c59fef0
devloper-kartik <112373403+devloper-kartik@users.noreply.github.com>
2022-08-30 15:11:13 +0530

    Local Storage in object

--- practice.js
+++ practice.js
@@ -1,61 +1,28 @@
-var form = document.getElementById("addForm");
-var itemList = document.getElementById("items");
-var filter  = document.getElementById("filter");
+var form = document.getElementById("my-form");
+var ul = document.getElementById("ul");
+form.addEventListener("submit",afterSub);
 
-form.addEventListener("submit",addfun);
-itemList.addEventListener("click",removeItem);
-filter.addEventListener("keyup",search);
+var obj = JSON.parse(localStorage.getItem("Myobj"));
 
-function addfun(e){
+function afterSub(e){
+
     e.preventDefault();
+    var username = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var obj = {
+        name : username,
+        e_mail : email,
+    }
+    localStorage.setItem("Myobj",JSON.stringify(obj));
+    // console.log(JSON.parse(localStorage.getItem("Myobj")));
 
-    var newItem =  document.getElementById('item').value;
-    var dis  = document.getElementById("disc").value;
-    var li = document.createElement("li");
-    var next = document.createElement("br");
-
-    li.className = "list-group-item";
-    li.appendChild(document.createTextNode(newItem));
-    li.appendChild(next);
-    li.appendChild(document.createTextNode(dis));
-    var delbtn = document.createElement("button");
-    var editbtn = document.createElement("button");
-
-    delbtn.className = "btn btn-danger btn-sm float-right delete";
-    delbtn.appendChild(document.createTextNode("X"));
-
-    editbtn.appendChild(document.createTextNode("Edit"));
-    editbtn.className = "btn btn-danger btn-sm float-right delete";
     
 
-    li.appendChild(delbtn);
-
-    itemList.appendChild(li);
-
-    li.insertBefore(editbtn,delbtn);
 }
 
-function removeItem(e){
-    if(e.target.classList.contains('delete')){
-        var parent = e.target.parentElement;
-        if(confirm("Are You Sure you want to remove "+parent.childNodes[0].textContent)){
-            itemList.removeChild(parent);
-        }
-    }
-}
-
-function search(e){
-    var text = e.target.value.toLowerCase();
-    var list = itemList.getElementsByTagName("li");
-
-    Array.from(list).forEach((item)=>{
-        // var name = item.firstChild.textContent;
-        var dis = item.childNodes;
-        console.log(dis);
-        if(dis[0].textContent.toLowerCase().indexOf(text) != -1 || dis[2].textContent.toLowerCase().indexOf(text) != -1 ){
-            item.style.display = 'block';
-        }else{
-            item.style.display = "none";
-        }
-    });
-}
+var li = document.createElement("li");
+    li.appendChild(document.createTextNode(obj.name));
+    li.appendChild(document.createElement("br"));
+    li.appendChild(document.createTextNode(obj.e_mail));
+    console.log(li);
+    ul.appendChild(li);
