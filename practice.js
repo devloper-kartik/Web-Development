7266f47cf17d2093094c0110518f8fa8492309a3
devloper-kartik <112373403+devloper-kartik@users.noreply.github.com>
2022-08-31 13:40:52 +0530

    add all users

--- practice.js
+++ practice.js
@@ -1,28 +1,66 @@
+// for(var i=0;i<localStorage.length;i++){
+//     localStorage.removeItem(localStorage.key(i));
+// }
+
 var form = document.getElementById("my-form");
 var ul = document.getElementById("ul");
 form.addEventListener("submit",afterSub);
 
-var obj = JSON.parse(localStorage.getItem("Myobj"));
+for(var i=0;i<localStorage.length;i++){
+    var obj = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(obj.name));
+    li.appendChild(document.createElement("br"));
+    li.appendChild(document.createTextNode(obj.e_mail));
+    var edit = document.createElement("button");
+    var del = document.createElement("button");
+    edit.appendChild(document.createTextNode('Edit'));
+    del.appendChild(document.createTextNode('Delete'));
+    li.appendChild(edit);
+    li.appendChild(del);
+    ul.appendChild(li); 
+
+}
+
 
 function afterSub(e){
-
     e.preventDefault();
+    console.log(localStorage.length);
     var username = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var obj = {
         name : username,
         e_mail : email,
     }
-    localStorage.setItem("Myobj",JSON.stringify(obj));
-    // console.log(JSON.parse(localStorage.getItem("Myobj")));
+    var store = localStorage.length+1
+    localStorage.setItem(store,JSON.stringify(obj));
 
-    
-
-}
-
-var li = document.createElement("li");
+    var obj = JSON.parse(localStorage.getItem(store));
+    var li = document.createElement("li");
     li.appendChild(document.createTextNode(obj.name));
     li.appendChild(document.createElement("br"));
     li.appendChild(document.createTextNode(obj.e_mail));
-    console.log(li);
-    ul.appendChild(li);
+    var edit = document.createElement("button");
+    var del = document.createElement("button");
+    edit.appendChild(document.createTextNode('Edit'));
+    del.appendChild(document.createTextNode('Delete'));
+    li.appendChild(edit);
+    li.appendChild(del);
+    ul.appendChild(li);     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
